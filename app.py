aaa881198f5e4fbe6feb054133fb85f7304116e0
Hazel <hazlann.ee@gmail.com>
2026-03-10 19:20:48 +0800

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,20 @@ import streamlit as st
 import pandas as pd
 from datetime import date
 
+# Example data
+chart_data = pd.DataFrame({
+    "Title": ["Kaya-chan wa Kowakunai", "The Greatest Estate Developer", "Tamen De Gushi"],
+    "Rating": [8, 10, 9]
+})
+
+# Altair bar chart
+chart = alt.Chart(chart_data).mark_bar(color="#82CAFF").encode(
+    x=alt.X("Title", sort=None),  # Keep order as in DataFrame
+    y="Rating"
+).properties(width=600, height=400)
+
+st.altair_chart(chart, use_container_width=True)
+###
 st.set_page_config(page_title="OtakuShelf", layout="wide")
 
 if "library" not in st.session_state:
@@ -64,20 +78,6 @@ elif page == "My Library":
 # statistics
 elif page == "Statistics":
     st.header("Reading Statistics")
-
-    # Example data
-chart_data = pd.DataFrame({
-    "Title": ["Kaya-chan wa Kowakunai", "The Greatest Estate Developer", "Tamen De Gushi"],
-    "Rating": [8, 10, 9]
-})
-
-# Altair bar chart
-chart = alt.Chart(chart_data).mark_bar(color="#82CAFF").encode(
-    x=alt.X("Title", sort=None),  # Keep order as in DataFrame
-    y="Rating"
-).properties(width=600, height=400)
-
-st.altair_chart(chart, use_container_width=True)
 
     total_series = len(st.session_state.library)
     completed = len(st.session_state.library[st.session_state.library["Status"] == "Completed"])
